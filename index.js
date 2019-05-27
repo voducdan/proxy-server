d000bae97a339bea9a6112a1fcb3904e7a82da36
Vo Duc Dan <voducdand99@gmail.com>
2019-05-27 10:51:22 +0700

    block/allow url from cmd

--- index.js
+++ index.js
@@ -7,23 +7,42 @@ const path = require('path')
 const cache = require('memory-cache')
 const Readable = require('stream').Readable;
 // khoi tao proxy server
+let mode = process.argv[2]
+let userUrl =process.argv[3]
+
+let blackList = fs.readFileSync('black_lists.conf', 'utf8').split('\r\n')
+if(mode !== undefined){
+    if(mode === 'allow'){
+        if(blackList.indexOf(userUrl) !== -1){
+            blackList.splice(blackList.indexOf(userUrl), 1)
+            fs.writeFileSync('black_lists.conf',blackList.join('\r\n'))
+            console.log('Allow url success')
+        }
+        else {
+            console.log('This url has not been blocked')
+        }
+    }
+    else {
+        if(blackList.indexOf(userUrl) !== -1){
+            console.log('This url already has been blocked')
+        }
+        else {
+            blackList.push(userUrl)
+            fs.writeFileSync('black_lists.conf', blackList.join('\r\n'))
+            console.log('Block url success')
+        }
+    }
+}
+
 http.createServer(onRequest).listen(8888)
 
-function staticServe(client_req, client_res) {
-    const staticPath = './static'
-    const resolvePath = path.resolve(staticPath)
-    console.log('res ' + resolvePath)
-    const normalizePath = path.normalize(client_req.url).replace(/^(\.\.[\/\\])+/, '')
-    console.log('nor ' + normalizePath)
-    const fileLoc = path.join(resolvePath, normalizePath)
-    console.log('file' + fileLoc)
-}
 
 function onRequest(client_req, client_res) {
 
     console.log('serve: ' + client_req.url)
+    
     // doc file black_list.conf de xem url vua nhap co hop le khong
-    let block = fs.readFileSync('./black_lists.conf', 'utf8').split('\r\n')
+    let block = fs.readFileSync('black_lists.conf', 'utf8').split('\r\n')
 
     // doc lai file neu co thay doi
     fs.watchFile('black_lists.conf', (cur, pre) => {
@@ -37,17 +56,18 @@ function onRequest(client_req, client_res) {
 
         // nguoc lai
     } else {
-        start = new Date()
+        // kiem tra url vua nhap da co trong cache chua
         if (cache.get(client_req.url)) {
+            // neu url co trong cache tra ve client ma khong gui request len server
             console.log(client_req.url)
             console.log('cache hit')
-            // const stream = new Readable();
-            client_res.writeHead(200)
 
-            // console.log(cache.get(client_req.url))
+            client_res.writeHead(200)
             client_res.write(cache.get(client_req.url))
             client_res.end()
+        // neu chua co trong cache
         } else {
+            // lay thong tin url ma user nhap
             var options = {
                 hostname: client_req.headers['host'],
                 port: client_req.port,
@@ -55,19 +75,20 @@ function onRequest(client_req, client_res) {
                 method: client_req.method,
                 headers: client_req.headers
             };
+            let buffer = '' // lay du lieu tu server de luu vao cache
             // gui request toi server ma user muon
-            let buffer = ''
+            // thong tin la thong tin lay o tren
             let proxy = http.request(options, function (res) {
+                // tra ve client header va status ma server phan hoi
                 client_res.writeHead(res.statusCode, res.headers)
-                // console.log(res.headers)
+                // du lieu tra ve proxy co the bi nen lai => can giai nen 
                 if (res.headers["content-encoding"] === 'gzip') {
                     const gunzip = zlib.createGunzip();
                     res.pipe(gunzip);
+                    // bat su kien data de lay du lieu
                     gunzip.on('data', async function (chunk) {
                         const reqUrl = client_req.url
-                        // if (reqUrl.match(/\.(ico|jpg|jpeg|png|gif|svg|js|css|php)/) === null) {
                         buffer += chunk
-                        // }
                     }).on("end", function () {
                         cache.put(client_req.url, buffer)
                         // console.log('buffer: ' + client_req.url + '  ' + buffer)
@@ -75,28 +96,40 @@ function onRequest(client_req, client_res) {
                     }).on("error", function (e) {
                         console.log(e)
                         client_res.writeHead(500)
+                    // bat su kien error khi co loi
                     }).on('error', function (e) {
                         console.log(e)
                         client_res.writeHead(500)
                     })
                 } else {
-                    res.on('data', data => {
-                        fs.writeFileSync('data.txt', data.toString())
-                    }).on('end', () => {})
-                    res.on('error', err => {
-                        console.log(err)
+                    // neu du lieu khong bi nen thi co the lam truc tiep
+                    res.on('data', async function (chunk) {
+                        const reqUrl = client_req.url
+                        buffer += chunk
+                    }).on("end", function () {
+                        cache.put(client_req.url, buffer)
+                        // console.log('buffer: ' + client_req.url + '  ' + buffer)
+                        client_res.end()
+                    }).on("error", function (e) {
+                        console.log(e)
+                        client_res.writeHead(500)
+                    // bat su kien error khi co loi
+                    }).on('error', function (e) {
+                        console.log(e)
+                        client_res.writeHead(500)
                     })
                 }
+                // server tra ve du lieu dang stream, truyen stream nay vao bien client_res de tra ve cho client
                 res.pipe(client_res, {
                     end: true
                 });
             });
+            //goi ham va truyen gia tri vao proxy
             client_req.pipe(proxy, {
                 end: true
             });
         }
     }
-    fs.writeFileSync('json.txt', cache.exportJson())
 }
 // // page for testing
 // // http://www.washington.edu/
